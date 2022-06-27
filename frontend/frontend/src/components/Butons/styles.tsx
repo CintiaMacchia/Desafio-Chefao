371208ec07786c9b3306b5ed4de0f68e6be91b54
zaida cueto <zcueto58@gmail.com>
2022-06-26 22:19:28 -0300

    configuração inicial

---
+++ frontend/frontend/src/components/Butons/styles.tsx
@@ -0,0 +1,8 @@
+import { Form } from "react-bootstrap";
+import styled from "styled-components";
+
+export const StyledFormGroup = styled(Form.Control)`
+    margin-top: 26px ;
+    border-radius: 10px;
+    text-align: center;
+`
