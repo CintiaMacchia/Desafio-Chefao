95492a2974de2a6d674e278a6e573d198230e8d3
Paulo Sabanae <psabanae3@gmail.com>
2022-07-01 19:32:16 -0400

    Criada página de Login

---
+++ frontend/src/components/LoginUI/index.tsx
@@ -0,0 +1,32 @@
+import * as S from './styles';
+import { Form } from 'react-bootstrap';
+
+const LoginForm: React.FC = () => {
+
+    return (
+        <S.Body>
+            <S.Container>  
+                <Form className='w-100'>
+                    <h2>Já possui cadastro? Faça o Login!</h2>
+                    <Form.Group>
+                        <S.StyledLabel>Email</S.StyledLabel>
+                        <S.StyledInput className='w-100' id='email' name='email' type='email' />
+                    </Form.Group>
+                    <Form.Group>
+                        <S.StyledLabel>Senha</S.StyledLabel>
+                        <S.StyledInput className='w-100' id='senha' name='senha' type='password' />
+                    </Form.Group>
+                    <a href='#' className='link'>Esqueceu a senha?</a>
+                    <Form.Group className='mt-4'>
+                        <S.StyledButtonOrange>Login</S.StyledButtonOrange>
+                    </Form.Group>
+                </Form>
+                <hr />
+                <h2>É novo por aqui? Cadastre-se!</h2>
+                <S.StyledButton>Cadastrar</S.StyledButton>
+            </S.Container>
+        </S.Body>
+    )
+}
+
+export default LoginForm;
---
+++ frontend/src/pages/Login/index.tsx
@@ -0,0 +1,9 @@
+import Login from '../../components/LoginUI';
+
+const LoginPage: React.FC = () => {
+    return (
+        <Login />
+    )
+}
+
+export default LoginPage;
