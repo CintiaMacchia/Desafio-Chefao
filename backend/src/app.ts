b0b80131c2f207422bbff8cb64dafd4e9f8e1e91
Julio H Moura <juliohmoura@pm.me>
2022-07-03 14:01:39 -0300

    Começando rotas e middlewares

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,14 @@
 import express from 'express'
 import db from './shared/infrastructure/database'
+import handleMiddleware from './shared/middlewares/hendleMiddleware'
+import routes from './shared/routes/index'
 
 const app = express();
+
 db.hasConection()
 
 app.use(express.json());
+app.use(routes)
+app.use(handleMiddleware)
 
 export default app
