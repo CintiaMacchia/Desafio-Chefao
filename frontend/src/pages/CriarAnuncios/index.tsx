de8ece0ee8f7db2d568477140f86e83d00193fce
Mateus Jorge Renhe <mateusrenhe@gmail.com>
2022-07-05 17:05:28 -0300

    Pagina criacao de anuncio

---
+++ frontend/src/pages/CriarAnuncios/index.tsx
@@ -0,0 +1,17 @@
+import { Footer } from "../../components/Footer"
+import FormAnuncio from "../../components/FormAnuncio"
+import Menu from "../../components/Header"
+
+// import { Container } from './styles';
+
+const Anuncio = () => {
+  return (
+    <>
+      <Menu />
+      <FormAnuncio />
+      <Footer />
+    </>
+  )
+}
+
+export default Anuncio
--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,33 +1,27 @@
-import React from 'react';
-import { BrowserRouter , Routes, Route,  } from 'react-router-dom'
-import Cadastro from './pages/Cadastro';
-import Home from './pages/Home';
-import Recipes from './pages/produtos';
-import RecipesDetail from './pages/RecipesDetail';
-
-
-
-
-
-
+import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Cadastro from "./pages/Cadastro"
+import Anuncio from "./pages/CriarAnuncios"
+import Home from "./pages/Home"
+import Recipes from "./pages/produtos"
+import RecipesDetail from "./pages/RecipesDetail"
 
 // import { Container } from './styles';
 
 const AppRoutes: React.FC = () => {
-
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/singUp" element={<Cadastro />} />
+        <Route path="/anuncio" element={<Anuncio />} />
 
-        <Route path="/" element={<Home/>} />
-        <Route path="/singUp" element={< Cadastro/>} />
-        <Route path="/receitas" element={<Recipes/>}>
-                <Route path=":id" element={<RecipesDetail />} />
-            </Route> 
-       
+        <Route path="/receitas" element={<Recipes />}>
+          <Route path=":id" element={<RecipesDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
 }
 
-export default AppRoutes;
+export default AppRoutes
