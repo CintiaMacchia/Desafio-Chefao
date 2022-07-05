b05cd27e3a3a36ef53588a498a127e5b90a9b775
Paulo Sabanae <psabanae3@gmail.com>
2022-07-05 13:31:36 -0400

    Ajusta do componente HeroBanner

--- frontend/src/components/HeroBanner/styles.ts
+++ frontend/src/components/HeroBanner/styles.ts
@@ -3,14 +3,19 @@ import HeroImage from '../assets/images/hero.png';
 
 export const HeroContainer = styled.div`
     width: 100%;
-    max-width: 1440px;
     height: 100vh;
+    margin: 0 auto;
+    background-color: #FFEEDE;
 
     @media (min-width: 650px) {
         display: flex;
         flex-flow: row nowrap;
         justify-content: center;
         align-items: center;
+    }
+
+    @media (min-width: 1100px) {
+        height: 75vh;
     }
 `
 
@@ -19,15 +24,18 @@ export const HeroTextContainer = styled.div`
     width: 100%;
     height: 60%;
     padding: 2rem;
+    font-family: 'Quicksand', sans-serif;
     color: #161616;
 
     h1 {
         font-size: 2rem;
+        font-weight: 700;
         margin-bottom: 2rem;
     }
 
     h3 {
         font-size: 1rem;
+        font-weight: 700;
         margin-bottom: 2rem;
     }
 
@@ -55,10 +63,15 @@ export const HeroTextContainer = styled.div`
     @media (min-width: 1100px) {
         padding: 5rem;
         width: 47%;
+        max-width: 700px;
 
         h1 {
             font-size: 3.5rem;
         }
+    }
+
+    @media (min-width: 1441px) {
+        max-width: 700px;
     }
 `
 
@@ -74,6 +87,10 @@ export const HeroImageContainer = styled.div`
     }
 
     @media (min-width: 1100px) {
-        width: 53%;
+        width: 53%;   
+    }
+
+    @media (min-width: 1441px) {
+        max-width: 740px;
     }
 `
