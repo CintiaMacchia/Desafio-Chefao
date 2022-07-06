01b2aecf8b30dc6a1d8166d825900e55c8300e2e
Cintia Macchia <macchiapaola@hotmail.com>
2022-07-06 14:06:47 -0300

    add multer e cloudinary

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,13 +2,18 @@ import express from 'express'
 import db from './shared/infrastructure/database'
 import handleMiddleware from './shared/middlewares/hendleMiddleware'
 import routes from './shared/routes/index'
+const upload = require('./shared/infrastructure/config')
 
 const app = express();
 
 db.hasConection()
 
 app.use(express.json());
-app.use(routes)
+app.use(express.urlencoded({ extended: true}));
+app.use(upload.any());
+//app.usea(express.static('./src/public'))
+
+app.use(routes);
 app.use(handleMiddleware)
 
 export default app
