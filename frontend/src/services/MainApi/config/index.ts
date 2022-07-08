a45a0f1b1c62ee68c50f200d3fc6a7e60fca680e
zaida cueto <zcueto58@gmail.com>
2022-07-08 10:19:18 -0300

    configuraçao  modeficado

--- frontend/src/services/MainApi/config/index.ts
+++ frontend/src/services/MainApi/config/index.ts
@@ -1,10 +1,10 @@
 import axios   from "axios";
 
-const baseApi = axios.create({{
- baseURL= "http://localhost:3333",
+const baseApi = axios.create({
+ baseURL: "http://localhost:3333",
  headers: {
    " content-type":"applcation/json",
  }
 })
 
-export baseApi
+export default baseApi
