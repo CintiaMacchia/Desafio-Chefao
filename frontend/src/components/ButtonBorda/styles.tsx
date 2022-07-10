9dbf75d90b6ebfaca60c6857dbddae44b574b447
Paulo Sabanae <psabanae3@gmail.com>
2022-07-10 08:28:54 -0400

    CSS navbar ajustado de acordo com o Figma

--- frontend/src/components/ButtonBorda/styles.tsx
+++ frontend/src/components/ButtonBorda/styles.tsx
@@ -2,14 +2,13 @@ import styled from 'styled-components';
 
 export const StyledButton = styled.button`
     background-color:#FF8B20;
-    width: 125px;
-   
     border-radius: 10px;
     border: 1px #FF8B20 solid;
-    padding: 0.375rem 0.75rem;
+    padding: 0.75rem 2.313rem;
+    font-family: Poppins;
     font-style: normal;
-font-weight: 600;
-font-size: 16px;
+    font-weight: 600;
+    font-size: 16px;
     color: #ffffff;
     &:hover {
         background-color: #ffffff;
--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -12,73 +12,53 @@ import ButonCheio from '../ButonCheio';
 function Menu() {
 
 	return (
-
 		<header>
-		
-				< div className="row ">
-					<div className="  shadow-sm p-3 mb-2bg-body rounded ">
+
+			<div className="row ">
+				<div className="  shadow-sm p-3 mb-2bg-body rounded ">
 					<div className="container-lg">
 						<div className="d-flex justify-content-end">
-							<a className="underline me-4 text-dark"><GoLocation className='mx-2 fs-5  icon-header' />Todos os Estados< IoIosArrowDown /> </a>
-							<a className=" underline text-dark" href='/perfil'><BsPerson className='mx-2 fs-5 icon-header' />Minha Conta </a>
-
+							<GoLocation className='mx-2 fs-5  icon-header' /><a className="underline me-4 text-dark">Todos os Estados< IoIosArrowDown /> </a>
+							<BsPerson className='mx-2 fs-5 icon-header' /><a className=" underline text-dark" href='/perfil'>Minha Conta </a>
 						</div>
 					</div>
 				</div>
-				
-					<div className="  container-lg  mb-4">
-					<div className=" container-lg">
-						<div  className=" d-flex justify-content-between " >
-							<div >
 
-								<div className="mt-3" >
-									<img src={logo} alt="logo" title='logo-do-site' />
-								</div>
-
-							</div>
-							<div className=" ">
-								<div className="d-flex "> <input className='barra-de-pesquisa d-flex mt-4 ' type="text" placeholder='Pesquise os materias didaticos que o seu filho precisa' />
-									<BiSearch className='icon-search mt-4' /></div>
-							</div>
-							<div className="boton-favorito-publicar">
-
-
-							</div>
-							<div className="mt-4">
-								<ButtonBorda typeButton="submit" text={"publicar"} />
-
-							</div>
-							<div className="mt-4">
-
-								<ButonCheio typeButton="submit" text={"Favoritos"} />
+				<div className="container-lg my-4">
+					<div className="d-flex justify-content-center gap-4 align-items-center" >
+						<div>
+							<div>
+								<img src={logo} alt="logo" title='logo-do-site' />
 							</div>
 						</div>
-
-					</div  >
-				</div>
-
-				<div className="   categorias">
-<div className='container'>
-<div className="col-md-12   d-flex justify-content-between ">
-
-						<a href="#" className=" underline">Todas as Categorias</a>
-						<a href="#" className="underline " >Lancheiras</a>
-
-						<a href="#" className="underline ">Listas Escolares</a>
-						<a href="#" className="underline">Livros</a>
-						<a href="#" className="underline">Papelaria</a>
-						<a href="#" className="underline">Uniformes</a>
-
+						<div>
+							<div className="d-flex "> <input className='barra-de-pesquisa d-flex' type="text" placeholder='Pesquise os materias didáticos que o seu filho precisa' />
+							<BiSearch className='icon-search' /></div>
+						</div>
+						<div>
+							<ButonCheio typeButton="button" text={"Favoritos"} />
+						</div>
+						<div>
+							<ButtonBorda typeButton="button" text={"Anunciar"} />
+						</div>
 					</div>
-</div>
-					
-
 				</div>
+			</div>
 
+			<div className="categorias">
+				<div className='container'>
+					<div className="col-md-12 d-flex justify-content-center categories-navbar">
+						<a href="#" className="link-nav underline">Todas as Categorias</a>
+						<a href="#" className="link-nav underline">Lancheiras</a>
+						<a href="#" className="link-nav underline">Listas Escolares</a>
+						<a href="#" className="link-nav underline">Livros</a>
+						<a href="#" className="link-nav underline">Papelaria</a>
+						<a href="#" className="link-nav underline">Uniformes</a>
+					</div>
+				</div>
 			</div>
 
 		</header>
-
 	)
 }
 
