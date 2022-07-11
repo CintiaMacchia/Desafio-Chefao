3a55bd00eb16ce93c583c578142bac7b2aae2f09
hannalm <hanna.machado@gmail.com>
2022-07-11 11:38:45 -0300

    make it better2

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,9 @@
 const app = require('./app')
 
-app.listen(3550, () => {
-    console.log('Servidor conectado')
-})
+const port = process.env.DB_PORT || 3350
+app.listen(port, () => console.log('Servidor no ar!'))
+
+// app.listen(3550, () => {
+//     console.log('Servidor conectado')
+// })
+
--- backend/src/shared/infrastructure/config/config.js
+++ backend/src/shared/infrastructure/config/config.js
@@ -10,17 +10,19 @@ module.exports = {
         "secret_key": process.env.SECRET_KEY
     },
     "test": {
-        "username": "root",
-        "password": null,
-        "database": "database_test",
-        "host": "127.0.0.1",
-        "dialect": "mysql"
+        "username": process.env.DB_USER,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_NAME,
+        "host": process.env.DB_HOST,
+        "dialect": process.env.DB_DIALECT,
+        "secret_key": process.env.SECRET_KEY
     },
     "production": {
-        "username": "root",
-        "password": null,
-        "database": "database_production",
-        "host": "127.0.0.1",
-        "dialect": "mysql"
+        "username": process.env.DB_USER,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_NAME,
+        "host": process.env.DB_HOST,
+        "dialect": process.env.DB_DIALECT,
+        "secret_key": process.env.SECRET_KEY
     }
 }
