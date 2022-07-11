143c93385d7b7f872ac6fa2888d06a15953998e5
Cintia Macchia <macchiapaola@hotmail.com>
2022-07-11 16:20:46 -0300

    add config cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,11 @@ const cors = require('cors')
 const app = express();
 
 
-app.use(cors());
+app.use(
+    cors({
+        origin: 'https://desafio-chefao-one.vercel.app/'
+    })
+)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(upload.any());
